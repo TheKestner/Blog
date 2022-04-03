4d9b28775991f9dcbb7c7b56f8da03800f9aa157
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-03 23:09:07 +0000

    Changed navbar title name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     <div>
     <Navbar bg="dark" variant="dark">
       <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand href="#home">The Kestner Blog</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#about">About</Nav.Link>

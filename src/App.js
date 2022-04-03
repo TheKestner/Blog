5fdaeb60c6b8f17d84d402a5b17c074e84cbcf38
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-03 23:08:19 +0000

    added bootstrap navbar with general links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
-      </header>
+    <div>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#about">About</Nav.Link>
+        <Nav.Link href="#blog">Blog</Nav.Link>
+        <Nav.Link href="#projects">Projects</Nav.Link>
+      </Nav>
+      </Container>
+  </Navbar>
     </div>
   );
 }
-
-export default App;

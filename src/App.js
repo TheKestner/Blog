7c724c36f2040c327eb655c2b32849ce4154bd71
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-04 00:09:56 +0000

    added base route paths for index

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App() {
       </Nav>
       </Container>
   </Navbar>
+  <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './routes/about';
+import Blog from './routes/blog';
+import Projects from './routes/projects';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}>
-
+      <Route path="about" element={<About />} />
+      <Route path='blog' element={<Blog />} />
+      <Route path='projects' element={<Projects />} />
       </Route>
     </Routes>
     </BrowserRouter>

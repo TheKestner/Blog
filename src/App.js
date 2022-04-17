57d2cfb16871ecd106a4ddcce803f6910934544e
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-17 23:28:09 +0000

    added dropdown for navbar for blogs. Added week 1-4 route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -12,8 +12,14 @@ export default function App() {
       <Navbar.Brand href="/">The Kestner Blog</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="about">About</Nav.Link>
-        <Nav.Link href="blog">Blog</Nav.Link>
         <Nav.Link href="projects">Projects</Nav.Link>
+        <NavDropdown title="Blog" id="basic-nav-dropdown">
+          <NavDropdown.Item href="blog">Weeks 1-4</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
       </Nav>
       </Container>
   </Navbar>

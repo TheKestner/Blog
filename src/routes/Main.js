a87d5b79c4c0626604a3896b0ece8ccbfa7c242a
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-18 01:04:57 +0000

    added background image to image folder. Added comments for possible style for image for background for only Main route

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import background from '../images/background.jpg'
 
 export default function Main() {
 
-
-  
+    // const myStyle={
+    //     backgroundImage: `url(${background})`,
+    //     marginTop:'-60px',
+    //     height:'100vh',
+    //     fontSize:'50px',
+    //     backgroundSize: 'cover',
+    //     backgroundRepeat: 'no-repeat',
+    // };
 
     return (
+    <div>
         <div>
-           <div className="mt-5 text-center hero"> 
-          <h1>Zachary Kestner</h1>
-        <h2 className="lead"> Full Stack Web Developer</h2>
-      </div>
-    </div>          
+            <div className="mt-5 text-center hero"> 
+                <h1>Zachary Kestner</h1>
+                <h2 className="lead"> Full Stack Web Developer</h2>
+            </div>
+        </div>
+    </div>      
     );
   }
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -10,12 +10,12 @@ export default function Projects() {
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="holder.js/100px180" />
                     <Card.Body>
-                        <Card.Title>Week 1</Card.Title>
+                        <Card.Title>Project 1</Card.Title>
                         <Card.Text>
                          Some quick example text to build on the card title and make up the bulk of
                             the card's content.
                         </Card.Text>
-                        <Button variant="primary">Week 1</Button>
+                        <Button variant="primary">Project 1</Button>
                     </Card.Body>
                 </Card> 
                 </Row>

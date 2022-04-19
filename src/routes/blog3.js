46caa6392fd9b1f26425765e16f9d280dc09948f
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-19 23:49:26 +0000

    adding week 12 to blog 3

--- src/routes/blog3.js
+++ src/routes/blog3.js
@@ -79,6 +79,28 @@ export default function Blog3() {
                    </details>
                </Container>
 
+               <Container fluid>
+                   <h2>Week 12</h2>
+                   <details>
+                       <summary>What are your thoughts about how far you have come? How has your Digital Literacy, Computational Thinking, and Adoption of Coding Culture increased</summary>
+                       I've found it can be really easy to minimize how far you really come if you don't take a step back to realize it. Before starting this bootcamp, I had basically no coding experience and only hardware experience. 
+                       One the very first things that we learned was Github for this bootcamp. At the time, that seemed like a challenge, but now we use it so often and easily with our projects. 
+                       Now we have learned so many things within both Front-end and Back-end. My ability to locate useful resources and how to tackle a problem has changed as well. The most helpful thing for me is to break down a project/problem into a much smaller pieces. 
+                       I've learned that it is much easier to tackle building something that way instead of trying to do it all at once. After taking a step back, I can say I am proud of my progress even though I felt like I had a really rough start. 
+                       I am proud that I did not give up even when I felt like I was not good enough. I will take what I've learned from this bootcamp and continue to learn even after.
+                   </details>
+
+                   <details>
+                       <summary>What are you struggling with the most in the Tech Stack so far and why?</summary>
+                       I believe that I may struggle more with the Front-End after getting to experience the Back-end. Alignment and styling aspect of the front-end is something that may get easier with more practice with it but I still struggle with it.
+                   </details>
+
+                   <details>
+                       <summary>What companies or people (local or not) would you like to hear talk during a Bootcamp lunch and learn?</summary>
+                       It would be nice to hear from some companies that primarily focus on the back-end as well for jobs. Anything that may be Database focused, but one I can think of locally is bitwerx. 
+                   </details>
+               </Container>
+
         </div>
     );
 }

83adee83a40dcf5e790e15db333cf433d886a508
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-05-09 00:22:06 +0000

    saving progress with adding splide feature for the prject route.

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,25 +1,91 @@
 import React from "react";
 import { CardGroup } from "react-bootstrap";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 import { Container, Row, Image, Card, Button } from "react-bootstrap";
 
 export default function Projects() {
     return(
-        <div>
-           <Container fluid>
-                <Row>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Project 1</Card.Title>
-                        <Card.Text>
-                         Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Project 1</Button>
-                    </Card.Body>
-                </Card> 
-                </Row>
-               </Container> 
-        </div>
+        <Container fluid="md">
+           <Splide aria-label="My Favorite Images"
+             options={ {
+                rewind: true,
+                perPage: 3,
+                perMove: 1,
+                gap: '20px',
+                pagination: false,
+              } }
+           >
+                <SplideSlide>
+                    <Card style={{ margin: 'auto' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Project 1</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">View</Button>
+                        </Card.Body>
+                    </Card>
+                </SplideSlide> 
+
+                    <SplideSlide>
+                    <Card style={{ margin: 'auto' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Project 1</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">View</Button>
+                        </Card.Body>
+                    </Card> 
+                </SplideSlide>
+
+                <SplideSlide>
+                    <Card style={{ margin: 'auto'}}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Project 1</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">View</Button>
+                        </Card.Body>
+                    </Card> 
+                </SplideSlide>
+
+                <SplideSlide>
+                    <Card style={{ margin: 'auto' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Project 1</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">View</Button>
+                        </Card.Body>
+                    </Card> 
+                </SplideSlide>
+
+                <SplideSlide>
+                    <Card style={{ margin: 'auto' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Project 1</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">View</Button>
+                        </Card.Body>
+                    </Card> 
+                </SplideSlide>
+            </Splide>
+        </Container>
     );
 }
